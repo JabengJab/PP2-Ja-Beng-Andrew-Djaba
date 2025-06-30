911992384e5210b44fc5e4673facb92928e8cbea
JabengJab <a.djaba@hotmail.com>
2025-06-30 19:03:54 +0200

    Add all game content with explanation to readme.md file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
